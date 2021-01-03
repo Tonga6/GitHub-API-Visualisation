0d0cdefe6f50ebbd0dbb9122936f46143e3bf320
Tonga6 <60393889+Tonga6@users.noreply.github.com>
2021-01-03 16:21:55 +0000

    added GET for repos

--- github-vis-js/app.js
+++ github-vis-js/app.js
@@ -22,13 +22,15 @@ function gitHubAccess(username) {
 
     // Create XMLHttpRequest objects
     const getUser = new XMLHttpRequest();
+    const getRepos = new XMLHttpRequest();
 
     // GitHub endpoints
+    const repos_url = `https://api.github.com/users/${username}/repos`;
     const user_url = `https://api.github.com/users/${username}`;
 
     // Open connections using GET request via URL endpoint
     getUser.open('GET', user_url, true);
-
+    getRepos.open('GET', repos_url, true);
     // Process response
     getUser.onload = function () {
         const data = JSON.parse(this.response);
@@ -52,6 +54,38 @@ function gitHubAccess(username) {
         ul.appendChild(li);
     }
     
+    getRepos.onload = function () {
+
+        // Parse API data into JSON
+        const data = JSON.parse(this.response);
+
+        // Loop over each object in data array
+        for (let i in data) {
+
+            // Get the ul with id of of userRepos
+            let ul = document.getElementById('userRepos');
+
+            // Create variable that will create li's to be added to ul
+            let li = document.createElement('li');
+
+            // Add Bootstrap list item class to each li
+            li.classList.add('list-group-item')
+
+            // Create the html markup for each li
+            li.innerHTML = (`
+                <p><strong>Repo:</strong> ${data[i].name}</p>
+                <p><strong>Description:</strong> ${data[i].description}</p>
+                <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
+                <p><strong>Main Language:</strong> ${data[i].language}</p>
+            `);
+
+            // Append each li to the ul
+            ul.appendChild(li);
+
+        }
+
+    }
     // Send the request to the server
     getUser.send();
+    getRepos.send();
 }
