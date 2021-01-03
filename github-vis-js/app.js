8fe6fd11591a01ffcf99e24bb4a59cbb3f9449a6
Tonga6 <60393889+Tonga6@users.noreply.github.com>
2021-01-03 15:20:21 +0000

    implemented search field to take user input

---
+++ github-vis-js/app.js
@@ -0,0 +1,18 @@
+// Get the GitHub username input form
+const gitHubForm = document.getElementById('gitHubForm');
+
+// Listen for submissions on GitHub username input form
+gitHubForm.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    // Get the GitHub username input field on the DOM
+    let usernameInput = document.getElementById('usernameInput');
+
+    // Get the value of the GitHub username input field
+    let gitHubUsername = usernameInput.value;
+
+    //test search function
+    console.log(gitHubUsername);
+
+})
