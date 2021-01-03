bfab31e71c1707781e8bfaffdfe56b3c6fe905e4
Tonga6 <60393889+Tonga6@users.noreply.github.com>
2021-01-03 16:04:40 +0000

    Added GET to user endpoint and displayed results

--- github-vis-js/app.js
+++ github-vis-js/app.js
@@ -13,6 +13,45 @@ gitHubForm.addEventListener('submit', (e) => {
     let gitHubUsername = usernameInput.value;
 
     //test search function
-    console.log(gitHubUsername);
+    gitHubAccess(gitHubUsername);
 
 })
+
+
+function gitHubAccess(username) {
+
+    // Create XMLHttpRequest objects
+    const getUser = new XMLHttpRequest();
+
+    // GitHub endpoints
+    const user_url = `https://api.github.com/users/${username}`;
+
+    // Open connections using GET request via URL endpoint
+    getUser.open('GET', user_url, true);
+
+    // Process response
+    getUser.onload = function () {
+        const data = JSON.parse(this.response);
+
+        // Get the ul with id of of userInfo
+        let ul = document.getElementById('userInfo');
+        // Create variable that will create li's to be added to ul
+        let li = document.createElement('li');
+        // Add Bootstrap list item class to each li
+        li.classList.add('list-group-item')
+
+        // Create the html markup for each li
+        li.innerHTML = (`
+                <p><strong>Name:</strong> ${data.name}</p>
+                <p><strong>Location:</strong> ${data.location}</p>
+                <p><strong>Public Repos:</strong> ${data.public_repos}</p>
+                <p><strong>Member Since:</strong> ${data.created_at}</p>
+            `);
+
+        // Append each li to the ul
+        ul.appendChild(li);
+    }
+    
+    // Send the request to the server
+    getUser.send();
+}
